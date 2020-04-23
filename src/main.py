ea2c782c17d664c8214c74d597e66a264271fc34
Bonaventura Pacileo <bonaventurapacileo@gmail.com>
2020-04-23 11:44:32 +0200

    preprocess v4

--- src/main.py
+++ src/main.py
@@ -36,11 +36,22 @@ def monitorDataLambda(data, context):
 
     csvUploader(table_data, data)
 
+#table_data.loc[table_data.scene_index==key, 'scene_index'] = scene_dict[key]
 
+
+def sceneDict(table):
+    '''add descr'''
+    scene_dict = {0: 'Menu', 1: 'Goalkeeping', 2: 'Touch object', 3: 'Fruit picking', 4: 'Gym time trials',
+                  5: 'Calibration', 6: 'Sorting'}
+
+    for key in scene_dict.keys():
+        table.loc[table.scene_index == key, 'scene_index'] = scene_dict[key]
+
+    return table['scene_index']
 
 def rotRescaler(val, D=360):
     '''add descr'''
-    return np.where(val > D / 2, val - D, val)
+    return (np.where(val > D / 2, -1*(val - D), 0),np.where(val > D / 2, 0, val))
 
 
 def valDivider(table, k=1000):
@@ -66,12 +77,13 @@ def metscoreCalc(scene, table):
 
     return metscore
 
-def tableReader(blob):
+def tableReader(blob, cols):
     '''add descr'''
 
-    cols = ['index', 'scene_index', 'time', 'ms_lastline', 'head_posx', "head_posy", "head_posz", "head_rotx",
+    if cols is None:
+        cols = ['index', 'scene_index', 'time', 'ms_lastline', 'head_posx', "head_posy", "head_posz", "head_rotx",
             "head_roty", "head_rotz", "right_posx", "right_posy", "right_posz", "right_rotx", "right_roty",
-            "right_rotz", "left_posx", "left_posy", "left_posz", "left_rotx", "left_roty", "left_rotz", ]
+            "right_rotz", "left_posx", "left_posy", "left_posz", "left_rotx", "left_roty", "left_rotz"]
 
     with open("/tmp/my-file.txt", "wb") as file_obj:
         blob.download_to_file(file_obj)
@@ -86,28 +98,38 @@ def tableProcess(table, data):
     '''data(dict): The Cloud Functions event payload.'''
 
     ### 2. divide /1000 ###
-    table.loc[:, 'head_posx':] = valDivider(table.loc[:, 'head_posx':])
+    table.loc[:, 4:22] = valDivider(table.loc[:, 4:22])
 
     ### 3. format time ###
-    table['time'] = pd.to_datetime(table['time'], format="%H:%M:%S").dt.time
+    table['time'] = pd.to_datetime(table['time'], format="%H:%M:%S").dt.time #do we really need this column?
 
     ### 4. create date ###
     table['date'] = pd.to_datetime(data['name'][12:22], format = "%d-%m-%Y")
 
     ### 5. create seconds ###
-    timedelta = pd.to_timedelta(table.time.astype(str))
-    diff = timedelta.diff().fillna(pd.Timedelta(seconds=0)) / 1e9
-    table['seconds'] = np.cumsum(diff).astype(int)
+    #on hold for now
+    #timedelta = pd.to_timedelta(table.time.astype(str))
+    #diff = timedelta.diff().fillna(pd.Timedelta(seconds=0)) / 1e9
+    #table['seconds'] = np.cumsum(diff).astype(int)
 
     ### 6. rescale rotations ###
     cols = ["head_rotx", "head_roty", "head_rotz", "right_rotx", "right_roty", "right_rotz",
             "left_rotx", "left_roty", "left_rotz"]
 
     for col in cols:
-        table[str(col + "_n")] = rotRescaler(table[col])
+        table[str(col + "_n")], table[col] = rotRescaler(table[col])
 
     ### 7. compute MET score at frame level ###
-    table['met_score'] = metscoreCalc('scene_index',table)
+    table['met_score'] = metscoreCalc('scene_index', table)
+
+    ### 8. replace scene_index with string names ###
+    table['scene_index'] = sceneDict(table)
+
+    ### 9. create id column ###
+    table['id'] = 1 #in production this should come from the quest
+
+    ### 10. drop unused columns ###
+    table = table.drop('index ms_lastline'.split(), axis=1)
 
     return table
 
--- src/test-blob.py
+++ src/test-blob.py
@@ -7,7 +7,14 @@ from GCSmanage import upload_blob
 
 #import subprocess
 #subprocess.call(['/Users/bonaventurapacileo/Documents/IS-DS/VR/keys/gcp-setup.sh'])
+id1=['monitorData 26-02-2020_id1.txt',
+'monitorData 27-02-2020_id1.txt',
+'monitorData 31-01-2020_id1.txt',
+'monitorData 06-03-2020_id1.txt',
+'monitorData 03-03-2020_id1.txt']
 
-upload_blob("physio-bucket", "/Users/bonaventurapacileo/Documents/IS-DS/VR/data/monitorData 20-03-2020.txt", "monitorData 20-03-2020.txt")
+#[upload_blob("physio-bucket", "/Users/bonaventurapacileo/Documents/IS-DS/VR/data/"+id, id) for id in id1]
 
 ###########
+id1 = ['monitorDataLabel 20-03-2020.txt']
+[upload_blob("physio-bucket", "/Users/bonaventurapacileo/Documents/IS-DS/VR/"+id, id) for id in id1]
